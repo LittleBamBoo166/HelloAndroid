645baa68863acf19f18caf3c1890e0d99a5078c2
Phi Thuong <chontennguoidungcuaban166@gmail.com>
2022-09-07 11:15:22 +0700

    w3-widget

---
+++ widgetw3/app/src/main/java/com/example/widget_w3/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.widget_w3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
