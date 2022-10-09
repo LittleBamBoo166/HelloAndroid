d0f4257e85a8390c49388965ce923d02ef45044c
Phi Thuong <chontennguoidungcuaban166@gmail.com>
2022-10-09 17:08:31 +0700

    Bai tap giao dien profile nguoi dung

---
+++ giaodienprofile083022/app/src/main/java/com/example/giao_dien_profile_083022/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.giao_dien_profile_083022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
