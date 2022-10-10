1355576343d071b2cb48315e3317deaf6c9cdb83
Phi Thuong <chontennguoidungcuaban166@gmail.com>
2022-10-10 11:08:15 +0700

    user profile 08-31-2022: list view

--- UserProfile083122/app/src/main/java/com/example/userprofile083122/MainActivity.java
+++ UserProfile083122/app/src/main/java/com/example/userprofile083122/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.userprofile083122;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+    MaterialButton signUpBtn;
+    EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = (EditText) findViewById(R.id.email_edit_text);
+        password = (EditText) findViewById(R.id.password_edit_text);
+        signUpBtn = (MaterialButton) findViewById(R.id.signup_btn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (email.getText().toString().equals("phithuong") && password.getText().toString().equals("phithuong")) {
+                    // correct
+                    Toast.makeText(MainActivity.this, "Login successfull", Toast.LENGTH_SHORT).show();
+                    Intent listActivity = new Intent(MainActivity.this, ListActivity.class);
+                    startActivity(listActivity);
+                } else {
+                    // incorrect
+                    Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
