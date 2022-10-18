935b1ccb1bb9a1f846a336307d70e4a6590074db
Phi Thuong <chontennguoidungcuaban166@gmail.com>
2022-10-18 08:21:10 +0700

    recycler view: oct 18

---
+++ RecyclerViewthoct18/app/src/main/java/com/example/recyclerview_th_oct_18/UserModel.java
@@ -0,0 +1,19 @@
+package com.example.recyclerview_th_oct_18;
+
+import java.io.Serializable;
+
+public class UserModel implements Serializable {
+    private String userName;
+
+    public UserModel(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
